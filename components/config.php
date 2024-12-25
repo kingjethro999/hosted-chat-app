fa2d104e45a44c22ead7de186f67753f22d5987d
King Jethro <jethrojerrybj@gmail.com>
2024-12-25 18:09:03 +0100

    first commit

---
+++ components/config.php
@@ -0,0 +1,30 @@
+<?php
+
+// Domain Name | Format: https://domain.tld
+$domain = "https://king.cc";
+
+// Site Name
+$site = "king.cc";
+
+// Cheat Loader Link
+$loader = "xaxa.exe";
+
+// Api key | Format ?username=&pass=&key=
+// Do not use same key
+$read_api = "key1";
+$insert_api = "key2";
+$service_api = "key3";
+
+// Footer Credits
+$author = "<a href='http://github.com/kingjethro999' target='_blank'>King</a>";
+$twitter = "@";
+
+// Logo Location | Format: .PNG
+$logo = "./assets/img/icon.png";
+//Favicon Location
+$favicon = "./assets/img/icon.png";
+// Website description
+$site_desc = "CS:GO Private Cheat";
+// Website keywords
+$site_keywords = "king.cc, kinghook, king, cheat, private cheat, csgo";
+?>
